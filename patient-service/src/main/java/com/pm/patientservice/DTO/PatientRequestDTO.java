e7674f2aa75c5e091e1945d31c7d7649855b3ce7
Kedarnath R Gubbi <kedarnathrgubbi@Kedarnaths-MacBook-Air.local>
2025-05-03 21:43:00 -0400

    first commit

---
+++ patient-service/src/main/java/com/pm/patientservice/DTO/PatientRequestDTO.java
@@ -0,0 +1,70 @@
+package com.pm.patientservice.DTO;
+
+import com.pm.patientservice.DTO.validators.CreatePatientValidation;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class PatientRequestDTO {
+    @NotBlank
+    @Size(max=100, message="Name cannot exceed 100 characters")
+    private String name;
+
+    @NotBlank
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Address is required")
+    private String address;
+
+    @NotBlank(message = "DOB cannot be blank")
+    private String dateOfBirth;
+
+    @NotBlank(groups = CreatePatientValidation.class, message = "Registered date is required")
+    private String registeredDate;
+
+
+    public String getRegisteredDate() {
+        return registeredDate;
+    }
+
+    public void setRegisteredDate(String registeredDate) {
+        this.registeredDate = registeredDate;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+}
